d8b1a35000e6009e1e538fa297ed1aad3eff8c42
Christoph Grabo <asaaki@mannaz.cc>
2022-12-02 07:16:05 +0100

    Refactor day 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -5,53 +5,33 @@ const BIN: &str = env!("CARGO_BIN_NAME");
 fn main() -> NullResult {
     let args = prepare_or_args(BIN)?;
 
-    // pre-compute inputs if needed and/or shared by solutions
-
-    let rounds: Vec<_> = args.input.iter().map(|l| {
-        let mut splits = l.splitn(2, ' ');
-        [
-            splits.next().unwrap().as_bytes()[0] as u32 - 64,
-            splits.next().unwrap().as_bytes()[0] as u32 - 87
-        ]
-    }).collect();
-
-
-    let solution: String = if !args.second {
-        let scores: Vec<_> = rounds.iter().map(|[elf, me]| {
-            // X 1 lose, Y 2 draw, Z 3 win
-            match (elf, me) {
-                (1 ,1) => 4,
-                (1 ,2) => 8,
-                (1 ,3) => 3,
-                (2 ,1) => 1,
-                (2 ,2) => 5,
-                (2 ,3) => 9,
-                (3 ,1) => 7,
-                (3 ,2) => 2,
-                (3 ,3) => 6,
-                (_, _) => 0
+    let scores = args
+        .input
+        .iter()
+        .map(|round| {
+            // … => [part1, part2]
+            // item + outcome
+            // item: rock=1, paper=2, scissor=3
+            // outcome: lose=0, draw=3, win=6
+            match (round.chars().nth(0), round.chars().nth_back(0)) {
+                (Some('A'), Some('X')) => [1 + 3, 3 + 0],
+                (Some('A'), Some('Y')) => [2 + 6, 1 + 3],
+                (Some('A'), Some('Z')) => [3 + 0, 2 + 6],
+                (Some('B'), Some('X')) => [1 + 0, 1 + 0],
+                (Some('B'), Some('Y')) => [2 + 3, 2 + 3],
+                (Some('B'), Some('Z')) => [3 + 6, 3 + 6],
+                (Some('C'), Some('X')) => [1 + 6, 2 + 0],
+                (Some('C'), Some('Y')) => [2 + 0, 3 + 3],
+                (Some('C'), Some('Z')) => [3 + 3, 1 + 6],
+                (_, _) => [0, 0],
             }
-        }).collect();
-        let part1: u32 = scores.iter().sum();
-        part1.to_string()
-    } else {
-        let plays: Vec<_> = rounds.iter().map(|[elf, me]| {
-            match (elf, me) {
-                (1 ,1) => 3+0,
-                (1 ,2) => 1+3,
-                (1 ,3) => 2+6,
-                (2 ,1) => 1+0,
-                (2 ,2) => 2+3,
-                (2 ,3) => 3+6,
-                (3 ,1) => 2+0,
-                (3 ,2) => 3+3,
-                (3 ,3) => 1+6,
-                (_, _) => 0
-            }
-        }).collect();
-        let part2: u32 = plays.iter().sum();
-        part2.to_string()
-    };
+        })
+        .reduce(|[score1, score2], [part1, part2]| {
+            [score1 + part1, score2 + part2]
+        })
+        .unwrap();
+
+    let solution = scores[args.second as usize];
 
     if args.example {
         example_output(&args, solution);
