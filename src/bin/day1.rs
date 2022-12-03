1044f15faae24fa7493fbc6d4f970ee9edc39919
Christoph Grabo <asaaki@mannaz.cc>
2022-12-03 07:58:46 +0100

    Avoid allocation (day 1)
    Sadly for part 2 still a Vec needed for sorting.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -8,19 +8,19 @@ fn main() -> NullResult {
     let args = args(BIN)?;
     let now = Instant::now();
 
-    let mut elves: Vec<Num> = args
-        .input
-        .split("\n\n")
-        .map(|bag| bag.split('\n').filter_map(|c| c.parse::<Num>().ok()).sum())
-        .collect();
-    elves.sort_unstable_by(|a, b| b.cmp(a));
+    let elves = args.input.split("\n\n").map(|bag| {
+        bag.split('\n')
+            .filter_map(|c| c.parse::<Num>().ok())
+            .sum::<Num>()
+    });
 
-    let mut it = elves.iter();
-
-    let solution = if !args.second {
-        it.next().unwrap().to_owned()
+    let solution: Num = if !args.second {
+        elves.max().unwrap().to_owned()
     } else {
-        it.take(3).sum()
+        // easiest way to get the top3
+        let mut elves: Vec<_> = elves.collect();
+        elves.sort_unstable_by(|a, b| b.cmp(a));
+        elves.iter().take(3).sum()
     };
 
     eprintln!("time: {:?}", now.elapsed());
