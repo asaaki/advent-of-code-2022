800d0b703a568a5e1b446c1a5824acada1300e32
Christoph Grabo <asaaki@mannaz.cc>
2022-12-03 07:12:05 +0100

    Use more functional style

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,61 +6,38 @@ fn main() -> NullResult {
     let args = args(BIN)?;
     let now = Instant::now();
 
-    let c2i = |v: char| {
-        let v = if v.is_lowercase() {
-            v as u32 - 'a' as u32 + 1
-        } else if v.is_uppercase() {
-            v as u32 - 'A' as u32 + 27
-        } else {
-            0
-        };
-        v
-    };
-
     let part1: Vec<_> = args
         .input
+        .trim_end()
         .split('\n')
-        .filter(|&l| !l.is_empty())
         .map(|s| {
-            // dbg!(l, l.len()/2);
             let (left, right) = s.split_at(s.len() / 2);
-            // todo not save
-            let mut v = '0';
-            for l in left.chars() {
-                for r in right.chars() {
-                    if l == r {
-                        v = l.clone();
-                        break;
-                    }
-                }
-            }
-            v
+            // invariant: both sides always have one common char
+            left.chars()
+                .reduce(|acc, c| if right.contains(c) { c } else { acc })
+                .unwrap()
         })
-        .map(c2i)
+        .map(char2int)
         .collect();
 
-    let part2: Vec<_> = args.input.split('\n').collect();
-    // dbg!(&part2);
+    let part2: Vec<_> = args.input.trim_end().split('\n').collect();
     let part2: Vec<_> = part2
-        .chunks(3)
+        .chunks_exact(3)
         .map(|group| {
-            // todo not save
-            let mut v = '0';
-            for e1 in group[0].chars() {
-                for e2 in group[1].chars() {
-                    for e3 in group[2].chars() {
-                        if e1 == e2 && e2 == e3 {
-                            v = e1.clone();
-                            break;
-                        }
+            // invariant: all three members always have one common char
+            group[0]
+                .chars()
+                .reduce(|acc, c| {
+                    if group[1].contains(c) && group[2].contains(c) {
+                        c
+                    } else {
+                        acc
                     }
-                }
-            }
-            v
+                })
+                .unwrap()
         })
-        .map(c2i)
+        .map(char2int)
         .collect();
-    dbg!(&part2);
 
     let solution: u32 = if !args.second {
         part1.iter().sum()
@@ -70,4 +47,14 @@ fn main() -> NullResult {
 
     eprintln!("time: {:?}", now.elapsed());
     result(&args, solution)
+}
+
+fn char2int(c: char) -> u32 {
+    if c.is_lowercase() {
+        c as u32 - 'a' as u32 + 1
+    } else if c.is_uppercase() {
+        c as u32 - 'A' as u32 + 27
+    } else {
+        0
+    }
 }
