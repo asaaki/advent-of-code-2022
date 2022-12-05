d004d5038dd76ae817341401f7def80686fce627
Christoph Grabo <asaaki@mannaz.cc>
2022-12-05 08:40:51 +0100

    Improve day 5
    - check max slot count in first line with peekable (no iter clone)
    - use .skip(1) to skip the number line, avoids numeric check each line
    - combine instruction retrieval and application into same loop
    - always use temporary set, use .reverse() for part 2
    
    Runtime changes from ~100 µs to ~ 40 µs. Yay! \o/

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -8,22 +8,16 @@ fn main() -> NullResult {
 
     let (stacks, instructions) = args.input.split_once("\n\n").unwrap();
 
-    let stacks = stacks.lines();
-    let max_slots = stacks
-        .clone()
-        .last()
-        .unwrap()
-        .matches(char::is_numeric)
-        .count();
+    let mut stacks = stacks.lines().peekable();
+    let max_slots = stacks.peek().unwrap().len() / 4 + 1;
 
     let mut workset = vec![vec![' '; max_slots * 2]; max_slots];
-    for (li, l) in stacks.rev().enumerate() {
-        if l.chars().all(|c| !c.is_numeric()) {
-            let x = &l[1..];
-            for (ci, c) in x.char_indices() {
-                if c.is_alphabetic() && ci % 4 == 0 {
-                    workset[ci / 4][li - 1] = c;
-                }
+
+    for (li, l) in stacks.rev().skip(1).enumerate() {
+        let x = &l[1..];
+        for (ci, c) in x.char_indices() {
+            if c.is_alphabetic() && ci % 4 == 0 {
+                workset[ci / 4][li] = c;
             }
         }
     }
@@ -31,32 +25,24 @@ fn main() -> NullResult {
         w.retain(|c| c.is_alphabetic());
     }
 
-    let instructions = instructions.lines();
-    let moves: Vec<_> = instructions
-        .map(|l| {
-            let m = l.replace(|c: char| c.is_alphabetic(), " ");
-            let mut m = m.trim().split_whitespace();
-            let amount: usize = m.next().unwrap().parse().unwrap();
-            let from: usize = m.next().unwrap().parse().unwrap();
-            let to: usize = m.next().unwrap().parse().unwrap();
-            [amount, from, to]
-        })
-        .collect();
+    let mut tmp_stack = Vec::new();
 
-    let mut tmp_c = Vec::new();
-    for [i, from, to] in moves {
-        if !args.second {
-            for _ in 0..i {
-                let c = workset[from - 1].pop().unwrap();
-                workset[to - 1].push(c);
-            }
-        } else {
-            let offset = workset[from - 1].len() - i;
-            for c in workset[from - 1].drain(offset..) {
-                tmp_c.push(c);
-            }
-            workset[to - 1].append(&mut tmp_c);
+    for l in instructions.lines() {
+        let mut m = l
+            .split_ascii_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok());
+        let amount = m.next().unwrap();
+        let from = m.next().unwrap();
+        let to = m.next().unwrap();
+
+        for _ in 0..amount {
+            let c = workset[from - 1].pop().unwrap();
+            tmp_stack.push(c);
         }
+        if args.second {
+            tmp_stack.reverse();
+        }
+        workset[to - 1].append(&mut tmp_stack);
     }
     let solution: String = workset.iter().filter_map(|w| w.last()).collect();
 
