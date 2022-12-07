7bea2c77b1718e6024026e5fbadaa6604ad2508a
Christoph Grabo <asaaki@mannaz.cc>
2022-12-07 15:42:25 +0100

    Flatten line processing
    Uses a single level of match with a few branch conditionals.
    Definitely cheaper than my if+if+match and if+match+if nestings.
    And more ergonomic and comprehensible.

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -8,66 +8,43 @@ const THRESHOLD: N = 100_000;
 const DEVICE_MAX: N = 70_000_000;
 const UPDATE: N = 30_000_000;
 
-enum Mode {
-    Cd,
-    Ls,
-}
-
 fn main() -> NullResult {
     let args = args(BIN)?;
     let now = Instant::now();
-
-    let terminal = args.input.lines();
-    let mut mode = Mode::Cd;
 
     // NOTE: needed capacity measured from **my** inputs; avoids re-allocations during processing
     let mut cwd = String::with_capacity(70);
     let mut map = HashMap::with_capacity(167);
 
-    for line in terminal {
-        if line.starts_with('$') {
-            if line.contains("cd") {
-                mode = Mode::Cd;
-                let dir = line.split_ascii_whitespace().skip(2).next().unwrap();
-                match dir {
-                    "/" => {
-                        // use ".", so we can use "/" as delimiter instead
-                        cwd.push('.');
-                    }
-                    ".." => {
-                        let i = cwd.rfind('/').unwrap();
-                        cwd.truncate(i);
-                    }
-                    dirname => {
-                        cwd.push('/');
-                        cwd.push_str(dirname);
-                    }
-                }
-            } else if line.contains("ls") {
-                mode = Mode::Ls;
-            } else {
-                panic!("invalid command");
+    for line in args.input.lines() {
+        match line {
+            "$ ls" => { /* noop*/ }
+            "$ cd /" => {
+                // use ".", so we can use "/" as delimiter instead
+                cwd.push('.');
             }
-        } else {
-            match mode {
-                Mode::Ls => {
-                    if !line.starts_with("dir") {
-                        let fsize: N = line
-                            .split_ascii_whitespace()
-                            .next()
-                            .unwrap()
-                            .parse()
-                            .unwrap();
-                        let dsize = map.entry(cwd.clone()).or_insert(0);
-                        *dsize += fsize;
-                        cwd.match_indices('/').for_each(|(i, _)| {
-                            let dsize =
-                                map.entry(cwd[0..i].to_owned()).or_insert(0);
-                            *dsize += fsize;
-                        });
-                    }
-                }
-                _ => panic!("shouldn't reach"),
+            "$ cd .." => {
+                let i = cwd.rfind('/').unwrap();
+                cwd.truncate(i);
+            }
+            _ if line.starts_with("$ cd") => {
+                cwd.push('/');
+                cwd.push_str(&line[5..]);
+            }
+            _ if line.starts_with("dir") => { /* noop*/ }
+            _ => {
+                let fsize: N = line
+                    .split_ascii_whitespace()
+                    .next()
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let dsize = map.entry(cwd.clone()).or_insert(0);
+                *dsize += fsize;
+                cwd.match_indices('/').for_each(|(i, _)| {
+                    let dsize = map.entry(cwd[0..i].to_owned()).or_insert(0);
+                    *dsize += fsize;
+                });
             }
         }
     }
