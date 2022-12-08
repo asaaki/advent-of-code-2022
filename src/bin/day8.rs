040bee567502c6463549ce4f836d30eecf51ec00
Christoph Grabo <asaaki@mannaz.cc>
2022-12-08 16:29:11 +0100

    Use input as bytes only
    No Vec, but slower now.

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -6,19 +6,15 @@ fn main() -> NullResult {
     let args = args(BIN)?;
     let now = Instant::now();
 
-    let x = args.input.lines().next().unwrap().len();
-    let y = args.input.lines().count();
+    let input = args.input.as_ref();
+    let x = input.lines().next().unwrap().len();
+    let y = input.lines().count();
     let edge_count = x as u32 * 2 + y as u32 * 2 - 4; // _ _ | | -4 for overlapping corners
 
-    let mut trees = Vec::with_capacity(y);
-    for line in args.input.lines() {
-        // SAFETY: data is ASCII numeric
-        // NOTE: let's not bother with moving them down to 0-9
-        trees.push(line.as_bytes());
-    }
+    let tt = &input.as_bytes();
 
     let solution = if !args.second {
-        trees[..y - 1].iter().enumerate().skip(1).fold(
+        tt.chunks(x + 1).take(y - 1).enumerate().skip(1).fold(
             edge_count,
             |vv, (ty, l)| {
                 l[..x - 1]
@@ -26,10 +22,14 @@ fn main() -> NullResult {
                     .enumerate()
                     .skip(1)
                     .fold(vv, |v, (tx, t)| {
-                        let top =
-                            trees[..ty].iter().map(|l| &l[tx]).all(|n| n < t);
-                        let bottom = trees[ty + 1..]
-                            .iter()
+                        let top = tt
+                            .chunks(x + 1)
+                            .take(ty)
+                            .map(|l| &l[tx])
+                            .all(|n| n < t);
+                        let bottom = tt
+                            .chunks(x + 1)
+                            .skip(ty + 1)
                             .map(|l| &l[tx])
                             .all(|n| n < t);
                         let left = l[..tx].iter().all(|n| n < t);
@@ -44,23 +44,22 @@ fn main() -> NullResult {
             },
         )
     } else {
-        trees[..y - 1]
-            .iter()
-            .enumerate()
-            .skip(1)
-            .fold(0, |ss, (ty, l)| {
+        tt.chunks(x + 1).take(y - 1).enumerate().skip(1).fold(
+            0,
+            |ss, (ty, l)| {
                 l[..x - 1]
                     .iter()
                     .enumerate()
                     .skip(1)
                     .fold(ss, |s, (tx, t)| {
-                        let top = trees[..ty].iter().map(|l| &l[tx]);
-                        let bottom = trees[ty + 1..].iter().map(|l| &l[tx]);
+                        let top = tt.chunks(x + 1).take(ty).map(|l| &l[tx]);
+                        let bottom =
+                            tt.chunks(x + 1).skip(ty + 1).map(|l| &l[tx]);
                         let left = l[..tx].iter();
                         let right = l[tx + 1..].iter();
 
-                        let (mut s_top, mut s_bottom, mut s_left, mut s_right) = (0,0,0,0);
-
+                        let (mut s_top, mut s_bottom, mut s_left, mut s_right) =
+                            (0, 0, 0, 0);
                         for n in top.rev() {
                             s_top += 1;
                             if n >= t {
@@ -93,7 +92,8 @@ fn main() -> NullResult {
                             s
                         }
                     })
-            })
+            },
+        )
     };
 
     result(solution, now.elapsed(), &args)
