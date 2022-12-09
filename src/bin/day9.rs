5d06d1c2058a94beade73cee88a95ca461dc859d
Christoph Grabo <asaaki@mannaz.cc>
2022-12-09 08:22:54 +0100

    Add day 9 solution
    It's getting slower and slower …

---
+++ src/bin/day9.rs
@@ -0,0 +1,54 @@
+use ahash::{HashSet, HashSetExt};
+use aoc_lib::*;
+
+const BIN: &str = env!("CARGO_BIN_NAME");
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+fn main() -> NullResult {
+    let args = args(BIN)?;
+    let now = Instant::now();
+
+    let mut visited: HashSet<Pos> = HashSet::new();
+    let mut head = Pos::default();
+    let mut tails = [Pos::default(); 9];
+
+    for instruction in args.input.lines() {
+        let (direction, amount) = instruction.split_once(' ').unwrap();
+        for _ in 0..amount.parse().unwrap() {
+            match direction {
+                "U" => head.y += 1,
+                "D" => head.y -= 1,
+                "L" => head.x -= 1,
+                "R" => head.x += 1,
+                _ => panic!("shall not happen"),
+            }
+            follow(&head, &mut tails[0]);
+
+            if !args.second {
+                visited.insert(tails[0]);
+            } else {
+                for i in 1..9 {
+                    let (left, right) = tails.split_at_mut(i);
+                    follow(&left[i - 1], &mut right[0]);
+                }
+                visited.insert(tails[8]);
+            }
+        }
+    }
+
+    result(visited.len(), now.elapsed(), &args)
+}
+
+fn follow(lead: &Pos, t: &mut Pos) {
+    let dx = lead.x - t.x;
+    let dy = lead.y - t.y;
+    if (dx.abs() + dy.abs() >= 2) && (dx.abs() > 1 || dy.abs() > 1) {
+        t.x += dx.signum();
+        t.y += dy.signum()
+    }
+}
